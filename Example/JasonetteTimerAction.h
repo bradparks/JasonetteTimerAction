2142d5b8766e3c6e4020ac89df943d7dbc32c73f
Gliechtenstein <ethan.gliechtenstein@gmail.com>
2016-11-18 22:42:11 -0500

    first commit

---
+++ Example/JasonetteTimerAction.h
@@ -0,0 +1,13 @@
+//
+//  JasonetteTimerAction.h
+//  JasonetteTimerAction
+//
+//  Created by Unknower on 11/18/16.
+//  Copyright © 2016 Gliechtenstein. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JasonetteTimerAction : NSObject
+
+@end
---
+++ Example/JasonetteTimerAction.m
@@ -0,0 +1,13 @@
+//
+//  JasonetteTimerAction.m
+//  JasonetteTimerAction
+//
+//  Created by Unknower on 11/18/16.
+//  Copyright © 2016 Gliechtenstein. All rights reserved.
+//
+
+#import "JasonetteTimerAction.h"
+
+@implementation JasonetteTimerAction
+
+@end
